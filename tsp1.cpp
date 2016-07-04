787c08fedecbde65152ec3a8de9aae294ff94e41
MayukoT <tsupita@MTanemura-no-MacBook-Pro.local>
2016-07-05 00:25:49 +0900

    without a strategy

--- tsp1.cpp
+++ tsp1.cpp
@@ -1,45 +1,83 @@
 #include<iostream>
 #include<map>
 #include<cstdio>
+#include<cerrno>
+#include<cmath>
 
+using namespace std;
+
+//where the point is
 struct Location {
   float x;
   float y;
 };
 
+int datanum =0;
 
-using namespace std;
+//open
+FILE* fileopen(char* name){
+  FILE* fp=std::fopen(name,"r");
+  if(!fp){
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+  return fp;
+}
 
-int main(int argc, char const *argv[]) {
+//calculate distance
+float distance(float ax, float ay, float bx, float by){
+  float dis = pow(ax-bx,2.0)+pow(ay-by,2.0);
+  dis= sqrt(dis);
+  return dis;
+}
 
-  map<int, Location> city;
-
-  FILE* fp=std::fopen(argv[1],"r");
-
+//readfile and make map
+void fileread(FILE* fp, map <int,Location> &m){
   float x=0;
   float y=0;
+  char dummy1, dummy2;
   int i=0;
-  while (fscanf(fp,"%f%f\n",&x,&y)){
+  fscanf(fp, "%c,%c",&dummy1, &dummy2);
+  while (fscanf(fp,"%f,%f\n",&x,&y)!=EOF){
     Location buf;
     buf.x= x;
     buf.y= y;
-    city.insert(pair<int,Location>(i,buf));
+    m.insert(pair<int,Location>(i,buf));
     i++;
+    datanum++;
   }
-  cout<<"read" <<endl;
-/*for (int j = 0; j < i; j++) {
-  cout<<city[j]<<endl;
-}*/
-
-  float result=0;
-//calc distance
-for (int k = 0; k < i; k++) {
-  float x1=0; //insert pair of x and y
-  float y1=0;
-  float x2=0;
-  float y2=0;
-  result+= ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 }
+
+//calculate total diatance
+float sumdist(map<int, Location> &m, int* order){
+  float tdis=0;
+  for(int i=0; i<datanum; i++){
+    tdis+= distance(m[order[i]].x, m[order[i]].y, m[order[i+1]].x, m[order[i+1]].y);
+    cout << tdis << endl;
+    cout << "/* message */" << endl;
+  }
+  return tdis;
+}
+
+//the order to pass the point
+int* makeorder(int* order){
+  for (int i = 0; i < datanum; i++) {
+    order[i]= i;
+  }
+  return order;
+}
+
+//main
+int main(int argc, char *argv[]) {
+
+  map<int, Location> city;
+
+  FILE* fp=fileopen(argv[1]);
+  fileread(fp, city);
+
+  int order[datanum];
+  float result = sumdist(city, makeorder(order));
+
   cout<< result << endl;
 
   fclose(fp);
